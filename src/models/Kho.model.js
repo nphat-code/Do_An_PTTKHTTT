ab64bce3a70c26cee3d65ee5bba3bf8336e98b85
Nguyen Phat <nguyenphat13112006@gmail.com>
2026-02-25 19:04:28 +0700

    add kho

--- src/models/Kho.model.js
+++ src/models/Kho.model.js
@@ -4,7 +4,9 @@ const sequelize = require('../config/database');
 const Kho = sequelize.define('Kho', {
     maKho: { type: DataTypes.STRING(20), primaryKey: true },
     tenKho: { type: DataTypes.STRING, allowNull: false },
-    diaChi: { type: DataTypes.TEXT }
+    diaChi: { type: DataTypes.TEXT },
+    loaiKho: { type: DataTypes.ENUM('Kho lưu trữ', 'Kho trưng bày', 'Kho bảo hành') },
+    trangThai: { type: DataTypes.BOOLEAN, defaultValue: true }
     // maCn FK
 }, { tableName: 'KHO', timestamps: false });
 
